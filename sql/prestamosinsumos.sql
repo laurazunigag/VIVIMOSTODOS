5043fb9dffc95a8cb68b0c8a973fb75e4e816f63
Marvyn Lopez <marvyn.lopez@correounivalle.edu.co>
2026-04-22 15:01:59 -0500

    Acomode y quite el error en los prestamos por la tablas sql

--- sql/prestamosinsumos.sql
+++ sql/prestamosinsumos.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS prestamos_insumos (
     id_inventario INT NOT NULL,
     cantidad INT NOT NULL,
     fecha_prestamo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fecha_devolucion TIMESTAMP NULL,
-    estado ENUM('activo', 'devuelto') DEFAULT 'activo',
+    fecha_devolucion_esperada DATE NULL,
+    fecha_uso DATE NOT NULL,
+    fecha_devolucion_real TIMESTAMP NULL,
+    estado ENUM('activo', 'pendiente_devolucion', 'devuelto') DEFAULT 'activo',
     FOREIGN KEY (id_apartamento) REFERENCES usuarios(id_apartamento),
     FOREIGN KEY (id_inventario) REFERENCES inventario(id_inventario)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
